f621230fa92a929c6b70deb1cdc46e1b6fc180a2
Evandro Andrade <evandro.andrade@cnec.br>
2016-10-21 11:35:02 -0200

    teste

--- application/views/menu.php
+++ application/views/menu.php
@@ -6,7 +6,7 @@
            <span class="icon-bar"></span>
            <span class="icon-bar"></span>
            </button>
-           <a class="navbar-brand" href="#">Evandro O Andrade</a>
+           <a class="navbar-brand" href="#">Evandro Oliveira Andrade</a>
     	</div>
       <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav">
