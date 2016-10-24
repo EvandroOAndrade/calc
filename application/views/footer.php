4a4aeaa4cb42ca2826e2cd00e0577dbdd7abc108
Evandro Andrade <evandroooandrade@gmail.com>
2016-10-24 16:33:21 -0200

    f

---
+++ application/views/footer.php
@@ -0,0 +1,3 @@
+<div class="panel panel-default">
+       <div style="text-align: center; color:#4682B4; font-size: 12px;" class="panel-footer"><b>Author: </b>evandroooandrade@gmail.com</div>
+  </div>
---
+++ application/views/top.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Calculadora</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/bootstrap-theme.min.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/jquery.toast.min.css">
+  <script rel="stylesheet" src="<?= base_url() ?>assets/js/jquery.min.js"></script>
+  <script type="text/javascript" src="<?= base_url() ?>assets/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="<?= base_url() ?>assets/js/jquery.toast.min.js"></script>
+  <script type="text/javascript" src="<?= base_url() ?>assets/js/helpers.js"></script>
+  <script type="text/javascript" src="<?= base_url() ?>assets/js/functions.js"></script>
+
+</head>
+<body>
